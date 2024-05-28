502f29d780b345bf6d4bf30773e3f9ea5f4c225b
I Kadek Naryasa <98317796+zeynaryasa@users.noreply.github.com>
2024-05-28 18:45:04 +0800

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,9 +1,9 @@
-<?php
-// config.php
-$requested_url = $_SERVER['REQUEST_URI'];
-$page_path = $_SERVER['DOCUMENT_ROOT'] . $requested_url;
+// <?php
+// // config.php
+// $requested_url = $_SERVER['REQUEST_URI'];
+// $page_path = $_SERVER['DOCUMENT_ROOT'] . $requested_url;
 
-if (!file_exists($page_path)) {
-    header("Location: 404.html");
-    exit();
-}
+// if (!file_exists($page_path)) {
+//     header("Location: 404.html");
+//     exit();
+// }
